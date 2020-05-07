d239ebfb9c2f9eaf3e0841693b37e4e9f9a784e2
maciexu <43970987+maciexu@users.noreply.github.com>
2020-05-06 22:31:25 -0400

    Create scatter.py

---
+++ scatter.py
@@ -0,0 +1,29 @@
+""" scatter """
+fig, ax = plt.subplots()
+
+# Add data: "co2" on x-axis, "relative_temp" on y-axis
+ax.scatter(climate_change['co2'], climate_change['relative_temp'])
+
+# Set the x-axis label to "CO2 (ppm)"
+ax.set_xlabel('CO2 (ppm)')
+
+# Set the y-axis label to "Relative temperature (C)"
+ax.set_ylabel('Relative temperature (C)')
+
+plt.show()
+
+
+""" Encoding time by coloring the time """
+fig, ax = plt.subplots()
+
+# Add data: "co2", "relative_temp" as x-y, index as color
+ax.scatter(climate_change["co2"], climate_change["relative_temp"], c=climate_change.index)
+
+# Set the x-axis label to "CO2 (ppm)"
+ax.set_xlabel("CO2 (ppm)")
+
+# Set the y-axis label to "Relative temperature (C)"
+ax.set_ylabel("Relative temperature (C)")
+
+plt.show()
+
