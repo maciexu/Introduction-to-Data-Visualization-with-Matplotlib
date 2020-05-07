daabe42ffb3954903306f19c1100cc461dc6a7b0
maciexu <43970987+maciexu@users.noreply.github.com>
2020-05-06 21:43:26 -0400

    Update Quantitative_comparison_Stat_visualization.py

--- Quantitative_comparison_Stat_visualization.py
+++ Quantitative_comparison_Stat_visualization.py
@@ -1,1 +1,30 @@
-"""    
+"""    Bar """
+fig, ax = plt.subplots()
+
+# Plot a bar-chart of gold medals as a function of country
+ax.bar(medals.index, medals["Gold"])
+
+# Set the x-axis tick labels to the country names
+ax.set_xticklabels(medals.index, rotation=90)
+
+# Set the y-axis label
+ax.set_ylabel("Number of medals")
+
+plt.show()
+
+
+""" Stacked bars """
+# Add bars for "Gold" with the label "Gold"
+ax.bar(medals.index, medals['Gold'], label='Gold')
+
+# Stack bars for "Silver" on top with label "Silver"
+ax.bar(medals.index, medals['Silver'], label='Silver', bottom = medals['Gold'])
+
+# Stack bars for "Bronze" on top of that with label "Bronze"
+ax.bar(medals.index, medals['Bronze'], label='Bronze', bottom = medals['Gold']+medals['Silver'])
+
+# Display the legend
+ax.legend()
+
+plt.show()
+
