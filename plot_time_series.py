a153a7c56d273c76b9533f9b41789d667379e5d8
maciexu <43970987+maciexu@users.noreply.github.com>
2020-05-06 21:20:57 -0400

    Update plot_time_series.py

--- plot_time_series.py
+++ plot_time_series.py
@@ -1,1 +1,131 @@
+# Import pandas as pd
+import pandas as pd
+
+# Read the data from file using read_csv
+# Use the parse_dates key-word argument to parse the "date" column as dates.
+# Use the index_col key-word argument to set the "date" column as the index.
+climate_change = pd.read_csv('climate_change.csv', parse_dates = ['date'], index_col = 'date')
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+
+# Add the time-series for "relative_temp" to the plot
+# ax.plot(df.index, y)  
+# Note: The DataFrame index is accessed through: climate_change.index.
+ax.plot(climate_change.index, climate_change['relative_temp'])
+
+# Set the x-axis label
+ax.set_xlabel('Time')
+
+# Set the y-axis label 
+ax.set_ylabel('Relative temperature (Celsius)')
+
+# Show the figure
+plt.show()
+
+
+"""
+Using a time index to zoom in
+
+When a time-series is represented with a time index, we can use this index for the x-axis when plotting. 
+We can also select a to zoom in on a particular period within the time-series using Pandas' indexing facilities.
+"""
+import matplotlib.pyplot as plt
+
+# Use plt.subplots to create fig and ax
+fig, ax = plt.subplots()
+
+# Create variable seventies with data from "1970-01-01" to "1979-12-31"
+seventies = climate_change['1970-01-01':'1979-12-31']
+
+# Add the time-series for "co2" data from seventies to the plot
+ax.plot(seventies.index, seventies["co2"])
+
+# Show the figure
+plt.show()
+
+
+""" ax2 = ax.twinx() """
+""" Plotting two variables
+If you want to plot two time-series variables that were recorded at the same times, you can add both of them to the same subplot.
+
+If the variables have very different scales, you'll want to make sure that you plot them in different twin Axes objects. 
+These objects can share one axis (for example, the time, or x-axis) while not sharing the other (the y-axis).
+
+To create a twin Axes object that shares the x-axis, we use the twinx method.
+"""
+import matplotlib.pyplot as plt
+
+# Initalize a Figure and Axes
+fig, ax = plt.subplots()
+
+# Plot the CO2 variable in blue
+ax.plot(climate_change.index, climate_change["co2"], color='blue')
+
+# Create a twin Axes that shares the x-axis
+ax2 = ax.twinx()
+
+# Plot the relative temperature in red
+ax2.plot(climate_change.index, climate_change["relative_temp"], color='red')
+
+plt.show()
+
+
+""" Defining a function that plots time-series data """
+# Define a function called plot_timeseries
+def plot_timeseries(axes, x, y, color, xlabel, ylabel):
+
+  # Plot the inputs x,y in the provided color
+  axes.plot(x, y, color=color)
+
+  # Set the x-axis label
+  axes.set_xlabel(xlabel)
+
+  # Set the y-axis label
+  axes.set_ylabel(ylabel, color=color)
+
+  # Set the colors tick params for y-axis
+  axes.tick_params('y', colors=color)
+
+
+fig, ax = plt.subplots()
+
+# Plot the CO2 levels time-series in blue
+plot_timeseries(ax, climate_change.index, climate_change['co2'], "blue", 'Time (years)', 'CO2 levels')
+
+# Create a twin Axes object that shares the x-axis
+ax2 = ax.twinx()
+
+# Plot the relative temperature data in red
+plot_timeseries(ax2, climate_change.index, climate_change['relative_temp'], "red", 'Time (years)', 'Relative temperature (Celsius)')
+
+plt.show()
+
+
+""" Annotating time-series data """
+fig, ax = plt.subplots()
+
+# Plot the relative temperature data
+ax.plot(climate_change.index, climate_change['relative_temp'])
+
+# Annotate the date at which temperatures exceeded 1 degree
+ax.annotate('>1 degree', xy = (pd.Timestamp('2015-10-06'), 1))
+
+plt.show()
+
+""" ========"""
+# Plot the CO2 levels time-series in blue
+plot_timeseries(ax, climate_change.index, climate_change["co2"], 'blue', "Time (years)", "CO2 levels")
+
+# Create an Axes object that shares the x-axis
+ax2 = ax.twinx()
+
+# Plot the relative temperature data in red
+plot_timeseries(ax2, climate_change.index, climate_change['relative_temp'], 'red', "Time (years)", "Relative temp (Celsius)")
+
+# Annotate the point with relative temperature >1 degree
+# Place the text ">1 degree" in x=pd.Timestamp('2008-10-06'), y=-0.2 pointing with a gray thin arrow to x=pd.Timestamp('2015-10-06'), y = 1.
+ax2.annotate(">1 degree", xy=(pd.Timestamp('2015-10-06'), 1), xytext=(pd.Timestamp('2008-10-06'), -0.2), arrowprops={'arrowstyle':'->', 'color':'gray'})
+
+plt.show()
 
